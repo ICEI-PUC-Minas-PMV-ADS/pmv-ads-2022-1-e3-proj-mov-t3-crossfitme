e68cf431c72cb309d4a2b3499599c8d6338e4602
Jean-Stefan <jeanss12@gmail.com>
2022-04-05 23:43:02 -0300

    add pag Cadastro ao StackNavigator

--- src/src/navigations/StackNavigator.js
+++ src/src/navigations/StackNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Navigation from '../components/Navigation';
 import Login from '../pages/Login';
+import Cadastro from '../pages/Cadastro';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const StackNavigator = () => {
         <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Group>
                 <Stack.Screen name='Login' component={Login} />
+                <Stack.Screen name='Cadastro' component={Cadastro} />
                 <Stack.Screen name='Navigation' component={Navigation} />
             </Stack.Group>
         </Stack.Navigator>
