a5b7550606501912d0c629eebdeb1cbdcd496d93
Jean-Stefan <jeanss12@gmail.com>
2022-04-05 23:01:28 -0300

    criação componente input

---
+++ src/src/components/Input.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-paper';
+
+const Input = (props) => {
+    return (
+        <TextInput
+            mode='outlined'
+            outlineColor='#fafafa'
+            activeOutlineColor='#747474'
+            label={props.label}
+            style={styles.input}
+        />
+    );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+    input: {
+        backgroundColor: '#eeeeee',
+        height: 50,
+        marginBottom: 15,
+    },
+});
