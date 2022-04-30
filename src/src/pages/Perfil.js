fc68bca1539a3bb374104c05f64df3f69188e3ed
Jean-Stefan <jeanss12@gmail.com>
2022-04-30 17:12:32 -0300

    ajustes no perfil

--- src/src/pages/Perfil.js
+++ src/src/pages/Perfil.js
@@ -1,24 +1,33 @@
-import { View, StyleSheet } from 'react-native';
-import { Text, Button } from 'react-native-paper';
+import {View, StyleSheet} from 'react-native';
+import {Text, Button} from 'react-native-paper';
 import Container from '../components/Container';
 import ProfileImage from '../components/ProfileImage';
-import { useUser } from '../contexts/UserContext';
-
+import {useUser} from '../contexts/UserContext';
 
 const Perfil = () => {
-
-    const { name } = useUser();
-    const { mail } = useUser();
-    const { endereco } = useUser();
-    const { nascimento } = useUser();
-    const { desde } = useUser();
-    const { setSigned } = useUser();
+    const {name} = useUser();
+    const {mail} = useUser();
+    const {endereco} = useUser();
+    const {nascimento} = useUser();
+    const {desde} = useUser();
+    const {setSigned} = useUser();
 
     return (
         <Container>
-            <View style = {styles.topContainer}>
+            <View style={styles.topContainer}>
                 <Text style={styles.textDescription}> {name} </Text>
-                <Button onPress={() => { { setSigned(false) } }} icon={'exit-run'} dark={true} style={styles.button} color='gray'>sair </Button>
+                <Button
+                    onPress={() => {
+                        {
+                            setSigned(false);
+                        }
+                    }}
+                    dark={true}
+                    style={styles.button}
+                    color='gray'
+                >
+                    sair{' '}
+                </Button>
             </View>
 
             <View style={styles.profileContainer}>
@@ -54,19 +63,19 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'flex-end',
-        margin: 8
+        margin: 8,
     },
     textTitle: {
-        fontSize: 20,
+        fontSize: 16,
         color: '#666666',
         textAlign: 'center',
     },
     textDescription: {
-        fontSize: 30,
+        fontSize: 24,
         fontFamily: 'Poppins-SemiBold',
         textAlign: 'center',
     },
     button: {
         alignSelf: 'center',
-    }
+    },
 });
