5f5bf20adb85926cc4c481db391dfca8de725f80
Jean-Stefan <jeanss12@gmail.com>
2022-05-25 21:41:25 -0300

    Filtro de avaliação por aluno adicionado

--- src/src/services/relatoriofisico.service.js
+++ src/src/services/relatoriofisico.service.js
@@ -1,54 +1,70 @@
-import API from '../services/Webapi.services'
-import { BASE_URL } from '../services/Urls';
-
+import API from '../services/Webapi.services';
+import {BASE_URL} from '../services/Urls';
 
 export const CriarRelatorio = async (param) => {
-    try{
-      return await API.post(`${BASE_URL}/660/relatorios`, param).then( 
-        response => {
-          return response.data;
-        },
-        error =>{
-          console.log(error);
-          return  null;
-        }
-      );
-    }catch(error){
-      console.log(error);
-      return null;
+    try {
+        return await API.post(`${BASE_URL}/660/relatorios`, param).then(
+            (response) => {
+                return response.data;
+            },
+            (error) => {
+                console.log(error);
+                return null;
+            },
+        );
+    } catch (error) {
+        console.log(error);
+        return null;
     }
-  }
+};
 
-  export const GetRelatorios = async () => {
-    try{
-      return await API.get(`${BASE_URL}/660/relatorios`).then( 
-        response => {
-          return response.data;
-        },
-        error =>{
-          console.log(error);
-          return  null;
-        }
-      );
-    }catch(error){
-      console.log(error);
-      return null;
+export const GetRelatorios = async () => {
+    try {
+        return await API.get(`${BASE_URL}/660/relatorios`).then(
+            (response) => {
+                return response.data;
+            },
+            (error) => {
+                console.log(error);
+                return null;
+            },
+        );
+    } catch (error) {
+        console.log(error);
+        return null;
     }
-  }
+};
 
-  export const deleteRelatorio = async (id) => {
-    try{
-      return await API.delete(`${BASE_URL}/660/relatorios/${id}`).then( 
-        response => {
-          return response.data;
-        },
-        error =>{
-          console.log(error);
-          return  null;
-        }
-      );
-    }catch(error){
-      console.log(error);
-      return null;
+export const GetRelatoriosAluno = async (id) => {
+    try {
+        return await API.get(`${BASE_URL}/660/relatorios/?alunoId=${id}`).then(
+            (response) => {
+                return response.data;
+            },
+            (error) => {
+                console.log(error);
+                return null;
+            },
+        );
+    } catch (error) {
+        console.log(error);
+        return null;
     }
-  }
+};
+
+export const deleteRelatorio = async (id) => {
+    try {
+        return await API.delete(`${BASE_URL}/660/relatorios/${id}`).then(
+            (response) => {
+                return response.data;
+            },
+            (error) => {
+                console.log(error);
+                return null;
+            },
+        );
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
