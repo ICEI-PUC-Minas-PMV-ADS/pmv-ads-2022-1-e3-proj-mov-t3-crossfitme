4d19d732c546f20a9ca5bf4bbe3152d819fd7a94
Jean-Stefan <jeanss12@gmail.com>
2022-05-25 21:42:14 -0300

    Uso do filtro de avaliação por aluno

--- src/src/pages/Avaliacoes.js
+++ src/src/pages/Avaliacoes.js
@@ -1,82 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, SafeAreaView, ScrollView } from 'react-native';
-import { Text, List, Card } from 'react-native-paper';
+import React, {useState, useEffect} from 'react';
+import {
+    View,
+    StyleSheet,
+    FlatList,
+    SafeAreaView,
+    ScrollView,
+} from 'react-native';
+import {Text, List, Card} from 'react-native-paper';
 
 import ProfileCard from '../components/ProfileCard';
 import FloatingIcon from '../components/FloatingIcon';
 import AvaliacaoCard from '../components/AvaliacaoCard';
 import FloatingButton from '../components/FloatingButton';
 
-import { useNavigation } from '@react-navigation/native';
-import { useIsFocused } from '@react-navigation/native';
-import { useUser } from '../contexts/UserContext';
-import { GetRelatorios } from '../services/relatoriofisico.service';
-
+import {useNavigation} from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
+import {useUser} from '../contexts/UserContext';
+import {
+    GetRelatorios,
+    GetRelatoriosAluno,
+} from '../services/relatoriofisico.service';
 
 const Avaliacoes = () => {
     const navigation = useNavigation();
     const isFocused = useIsFocused();
-    const { rule } = useUser();
-    const { name } = useUser();
+    const {rule} = useUser();
+    const {name} = useUser();
+    const {id} = useUser();
 
     const [relatorio, setRelatorios] = useState([]);
 
     useEffect(() => {
-        GetRelatorios().then((dados) => {
-            setRelatorios(dados);
-        });
+        {
+            rule == 'admin'
+                ? GetRelatorios().then((dados) => {
+                      setRelatorios(dados);
+                  })
+                : GetRelatoriosAluno(id).then((dados) => {
+                      setRelatorios(dados);
+                  });
+        }
     }, [isFocused]);
 
-    const renderItem = ({ item }) => (
-        <AvaliacaoCard data={item.data} nome={item.name}  
-        onPress={() => navigation.navigate('RelatorioAvaliacao', {item: item})}
-       /> 
-       );
-    
+    const renderItem = ({item}) => (
+        <AvaliacaoCard
+            data={item.data}
+            nome={item.name}
+            onPress={() =>
+                navigation.navigate('RelatorioAvaliacao', {item: item})
+            }
+        />
+    );
+
     return (
-
-
         <View style={styles.container}>
             {/* <SafeAreaView>
                 <ScrollView > */}
-                    <View style={{ marginBottom: 20 }}>
-                        <ProfileCard
-                            name={name}
-                            source={require('../../assets/img/profile.jpg')}
-                        />
-                    </View>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            marginBottom: 20,
-                        }}
-                    >
-                        <FloatingIcon name={'file-document-outline'} />
-                        <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 22 }}>
-                            Avaliações Físicas
-                        </Text>
-                    </View>
-                    <View style={styles.flatList}>
-                        <FlatList
-                            data={relatorio}
-                            renderItem={renderItem}
-                            keyExtractor={(item) => item.id}
-                            // showsVerticalScrollIndicator={false}
-                        />
-                    </View>
-                    
-                {/* </ScrollView>
+            <View style={{marginBottom: 30}}>
+                <ProfileCard
+                    name={name}
+                    source={require('../../assets/img/profile.jpg')}
+                />
+            </View>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginBottom: 20,
+                }}
+            >
+                <FloatingIcon name={'file-document-outline'} />
+                <Text style={{fontFamily: 'Poppins-SemiBold', fontSize: 22}}>
+                    Avaliações Físicas
+                </Text>
+            </View>
+            <View style={styles.flatList}>
+                <FlatList
+                    data={relatorio}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+                    // showsVerticalScrollIndicator={false}
+                />
+            </View>
+
+            {/* </ScrollView>
             </SafeAreaView> */}
 
-            {rule == 'aluno' ? null : <FloatingButton
-                onPress={() => navigation.navigate('CadastroAvaliacao')}
-            />}
-
+            {rule == 'aluno' ? null : (
+                <FloatingButton
+                    onPress={() => navigation.navigate('CadastroAvaliacao')}
+                />
+            )}
         </View>
-
-
     );
 };
 
